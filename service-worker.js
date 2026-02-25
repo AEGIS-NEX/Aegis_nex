87c30fe1f60b5ba2e978373621ccea0140cfd0de
AEGIS <sintonreizad@gmail.com>
2026-02-25 07:06:57 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'aegis-nex-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/login.html',
+  '/register.html',
+  '/profile.html',
+  '/withdraw.html',
+  '/topup.html',
+  '/transfer.html',
+  '/history.html',
+  '/support.html',
+  '/notification.html',
+  '/kyc.html',
+  '/change_password.html',
+  '/change_pin.html',
+  '/recovery.html',
+  '/general_trade_hybrid.html',
+  '/admin.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-72.png',
+    data: {
+      url: data.url
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
